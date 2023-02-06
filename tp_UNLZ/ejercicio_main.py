8e5881b5335025cff744fabba411e5cb4b529a5d
Camila1548 <luistrotta@gmail.com>
2023-02-06 07:21:41 -0300

    ejercicios resueltos

--- tp_UNLZ/ejercicio_main.py
+++ tp_UNLZ/ejercicio_main.py
@@ -9,16 +9,16 @@ import ejercicio
 
 import pandas as pd
 
-#%%
-cargas_mineria =pd.read_excel('Matrices/Matrices Grupo Mineria_tp.xlsx',
-                              sheet_name='Total Toneladas Mineria 2014', usecols=(range(1,5)))
+carga_camion_mineria = pd.DataFrame(ejercicio.lista_camion)
+carga_camion_mineria.to_excel('Matrices/Carga_Mineria_Aderivar.xlsx')
 
-#%%
+cargas_mineria = pd.read_excel('Matrices/Matrices Grupo Mineria_tp.xlsx',sheet_name='Total Toneladas Mineria 2014', usecols=(range(1,5)))
+
 criterio = pd.read_excel('Matrices/Criterios de derivabilidad_tp.xlsx', sheet_name='MINERIA') 
 
 
 
-#%%
+
 def calcular_derivabilidad(lista_cargas, derivabilidad):
     """ Lee una lista de bibliotecas con informacion de cargas, origen, destino y distancia
     y utiliza criterios de derivavilidad al FFCC para crear una lista nueva similar pero
@@ -84,3 +84,6 @@ def calcular_derivabilidad(lista_cargas, derivabilidad):
     return derivable 
 
 lista_derivable = calcular_derivabilidad(ejercicio.lista_camion, criterio)
+
+df = pd.DataFrame(lista_derivable)
+df.to_excel('Matrices/Derivabilidad_mineria_3.xlsx')
